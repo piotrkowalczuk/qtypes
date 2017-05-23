6baf27b5d0e930cbda222e18e4d0ebac2456306d
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2017-05-23 19:50:49 +0200

    pypi integration

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+VERSION?=$(shell git describe --tags --always --dirty)
 PROTOC=/usr/local/bin/protoc
 
 .PHONY: proto
 
 proto:
-	@${PROTOC} -I=/usr/include -I=${GOPATH}/src -I=. --go_out=. qtypes.proto
+	@${PROTOC} -I=/usr/include -I=. --go_out=. qtypes.proto
+	@python -m grpc_tools.protoc -I=/usr/include -I=. --python_out=. --grpc_python_out=. qtypes.proto
 	@ls -al | grep "pb.go"
+	@ls -al | grep "_pb2"
+
+version:
+	@echo ${VERSION} > VERSION.txt
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from subprocess import check_output
+
+with open('VERSION.txt', 'r') as content_file:
+    version = content_file.read()
+
+    setup(
+        name='qtypes',
+        version=version[1:],
+        description='qtypes data structures',
+        url='http://github.com/piotrkowalczuk/qtypes',
+        author='Piotr Kowalczuk',
+        author_email='p.kowalczuk.priv@gmail.com',
+        license='MIT',
+        packages=[''],
+        install_requires=[
+            'protobuf'
+        ],
+        zip_safe=False,
+        keywords=['protobuf', 'data-structures'],
+        download_url='https://github.com/piotrkowalczuk/qtypes/archive/%s.tar.gz' % version.rstrip(),
+      )
